02d203304b0d1ad70884c4321973c86a987d172d
Hugo Aguirre <bul.ikana@gmail.com>
2018-02-24 22:29:39 -0600

    Penguin platform

---
+++ docs/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1519532362,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background.png",
+		"images/tiles-sheet0.png",
+		"images/tiles-sheet1.png",
+		"images/tiles-sheet2.png",
+		"images/enemy-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"loading-logo.png",
+		"icon-256.png"
+	]
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Penguin Platform
+
+A simple platform demo level made using using [Construct2](https://www.scirra.com/construct2).
+
+Move and jump with the arrow keys.
+
+You can test check it [here](https://bul-ikana.github.io/penguin-platform/).
